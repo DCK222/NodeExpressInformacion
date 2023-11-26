41525148b3365aa429ba4a8607e61e8d241af93f
DCK222 <alegard22@gmail.com>
2023-11-26 23:05:45 +0100

    NodeExpressInfo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./header/header.component";
+import { BodyComponent } from "./body/body.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css'],
+    imports: [CommonModule, RouterOutlet, HeaderComponent, BodyComponent]
 })
 export class AppComponent {
   title = 'NodeExpresAlejandroGarcia';
---
+++ src/assets/app.js
@@ -0,0 +1,15 @@
+document.getElementById('AbrirNode').addEventListener('click', function () {
+    document.getElementById('ventananode').style.display = 'block';
+});
+
+document.getElementById('cerrarexpressboton').addEventListener('click', function () {
+    document.getElementById('ventananode').style.display = 'none';
+});
+
+document.getElementById('AbrirExpress').addEventListener('click', function () {
+    document.getElementById('ventanexpress').style.display = 'block';
+});
+
+document.getElementById('cerrarexpress').addEventListener('click', function () {
+    document.getElementById('ventanexpress').style.display = 'none';
+});
